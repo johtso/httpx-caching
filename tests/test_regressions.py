f5d909107ea2869963c3b726ecbd4f37e0ae6b3c
Johannes <johtso@gmail.com>
2020-09-20 12:44:01 +0100

    Remove file cache related regression test

--- tests/test_regressions.py
+++ tests/test_regressions.py
@@ -2,27 +2,9 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-import sys
 import pytest
 
-
-from cachecontrol import CacheControl
-from cachecontrol.caches import FileCache
 from cachecontrol.filewrapper import CallbackFileWrapper
-from requests import Session
-
-
-class Test39(object):
-
-    @pytest.mark.skipif(
-        sys.version.startswith("2"), reason="Only run this for python 3.x"
-    )
-    def test_file_cache_recognizes_consumed_file_handle(self):
-        s = CacheControl(Session(), FileCache("web_cache"))
-        s.get("http://httpbin.org/cache/60")
-        r = s.get("http://httpbin.org/cache/60")
-        assert r.from_cache
-        s.close()
 
 
 def test_getattr_during_gc():
