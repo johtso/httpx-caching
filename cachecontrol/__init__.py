16efe0c66c7f1c5cdf1d94af24c10b0095f95031
Johannes <johtso@gmail.com>
2020-09-20 09:27:22 +0100

    Remove wrapper and refactor test_adapter

--- cachecontrol/__init__.py
+++ cachecontrol/__init__.py
@@ -10,6 +10,5 @@ __author__ = "Eric Larson"
 __email__ = "eric@ionrock.org"
 __version__ = "0.12.6"
 
-from .wrapper import CacheControl
-from .adapter import CacheControlAdapter
+from .transport import CacheControlTransport
 from .controller import CacheController
--- tests/test_adapter.py
+++ tests/test_adapter.py
@@ -5,64 +5,47 @@
 import mock
 import pytest
 
-from requests import Session
-from cachecontrol.adapter import CacheControlAdapter
+from httpx import Client
+from cachecontrol.transport import CacheControlTransport
 from cachecontrol.cache import DictCache
-from cachecontrol.wrapper import CacheControl
 
 
-def use_wrapper():
-    print("Using helper")
-    sess = CacheControl(Session())
-    return sess
+@pytest.fixture
+def client(url):
+    client = Client(transport=CacheControlTransport())
+    client.get(url)
+    yield client
+    client.close()
 
 
-def use_adapter():
-    print("Using adapter")
-    sess = Session()
-    sess.mount("http://", CacheControlAdapter())
-    return sess
+class TestClientActions(object):
 
-
-@pytest.fixture(params=[use_adapter, use_wrapper])
-def sess(url, request):
-    sess = request.param()
-    sess.get(url)
-    yield sess
-
-    # closing session object
-    sess.close()
-
-
-class TestSessionActions(object):
-
-    def test_get_caches(self, url, sess):
-        r2 = sess.get(url)
+    def test_get_caches(self, url, client):
+        r2 = client.get(url)
         assert r2.from_cache is True
 
-    def test_get_with_no_cache_does_not_cache(self, url, sess):
-        r2 = sess.get(url, headers={"Cache-Control": "no-cache"})
+    def test_get_with_no_cache_does_not_cache(self, url, client):
+        r2 = client.get(url, headers={"Cache-Control": "no-cache"})
         assert not r2.from_cache
 
-    def test_put_invalidates_cache(self, url, sess):
-        r2 = sess.put(url, data={"foo": "bar"})
-        sess.get(url)
+    def test_put_invalidates_cache(self, url, client):
+        r2 = client.put(url, data={"foo": "bar"})
+        client.get(url)
         assert not r2.from_cache
 
-    def test_patch_invalidates_cache(self, url, sess):
-        r2 = sess.patch(url, data={"foo": "bar"})
-        sess.get(url)
+    def test_patch_invalidates_cache(self, url, client):
+        r2 = client.patch(url, data={"foo": "bar"})
+        client.get(url)
         assert not r2.from_cache
 
-    def test_delete_invalidates_cache(self, url, sess):
-        r2 = sess.delete(url)
-        sess.get(url)
+    def test_delete_invalidates_cache(self, url, client):
+        r2 = client.delete(url)
+        client.get(url)
         assert not r2.from_cache
 
     def test_close(self):
         cache = mock.Mock(spec=DictCache)
-        sess = Session()
-        sess.mount("http://", CacheControlAdapter(cache))
+        client = Client(transport=CacheControlTransport(cache))
 
-        sess.close()
+        client.close()
         assert cache.close.called
