c42731eb9231cfeca3e873741eddece8fb69c111
Johannes <johtso@gmail.com>
2020-09-20 09:45:19 +0100

    Refactor test_chunked_response

--- tests/test_chunked_response.py
+++ tests/test_chunked_response.py
@@ -7,29 +7,27 @@ Test for supporting streamed responses (Transfer-Encoding: chunked)
 from __future__ import print_function, unicode_literals
 
 import pytest
-import requests
+from httpx import Client
 
-from cachecontrol import CacheControl
+from cachecontrol import CacheControlTransport
 
 
 @pytest.fixture()
-def sess():
-    sess = CacheControl(requests.Session())
-    yield sess
-
-    # closing session object
-    sess.close()
+def client():
+    client = Client(transport=CacheControlTransport())
+    yield client
+    client.close()
 
 
 class TestChunkedResponses(object):
 
-    def test_cache_chunked_response(self, url, sess):
+    def test_cache_chunked_response(self, url, client):
         """
         Verify that an otherwise cacheable response is cached when the
         response is chunked.
         """
         url = url + "stream"
-        r = sess.get(url)
+        r = client.get(url)
         from pprint import pprint
 
         pprint(dict(r.headers))
@@ -37,22 +35,22 @@ class TestChunkedResponses(object):
         print(r.content)
         assert r.headers.get("transfer-encoding") == "chunked"
 
-        r = sess.get(url, headers={"Cache-Control": "max-age=3600"})
+        r = client.get(url, headers={"Cache-Control": "max-age=3600"})
         assert r.from_cache is True
 
-    def test_stream_is_cached(self, url, sess):
-        resp_1 = sess.get(url + "stream")
+    def test_stream_is_cached(self, url, client):
+        resp_1 = client.get(url + "stream")
         content_1 = resp_1.content
 
-        resp_2 = sess.get(url + "stream")
+        resp_2 = client.get(url + "stream")
         content_2 = resp_1.content
 
         assert not resp_1.from_cache
         assert resp_2.from_cache
         assert content_1 == content_2
 
-    def test_stream_is_not_cached_when_content_is_not_read(self, url, sess):
-        sess.get(url + "stream", stream=True)
-        resp = sess.get(url + "stream", stream=True)
+    def test_stream_is_not_cached_when_content_is_not_read(self, url, client):
+        client.stream(url + "stream")
+        resp = client.stream(url + "stream")
 
         assert not resp.from_cache
