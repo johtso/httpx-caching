63747c4662a7beb164d53d429b951cc769a3c4b9
Johannes <johtso@gmail.com>
2020-09-22 21:07:58 +0100

    Strip out project specific stuff

--- cachecontrol/__init__.py
+++ cachecontrol/__init__.py
@@ -1,14 +1,7 @@
-# SPDX-FileCopyrightText: 2015 Eric Larson
-#
-# SPDX-License-Identifier: Apache-2.0
-
 """CacheControl import Interface.
 
 Make it easy to import from cachecontrol without long namespaces.
 """
-__author__ = "Eric Larson"
-__email__ = "eric@ionrock.org"
-__version__ = "0.12.6"
 
 from .adapter import SyncHTTPCacheTransport
 from .controller import CacheController
