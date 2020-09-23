6e9fa69013ded5c94fd35c4e34ec0e74ce399255
Johannes <johtso@gmail.com>
2020-09-23 18:02:24 +0100

    Cache control tests passing

--- tests/test_cache_control.py
+++ tests/test_cache_control.py
@@ -9,8 +9,12 @@ import pytest
 from mock import ANY, Mock
 import time
 
+import httpcore
+from httpx import Headers
+
 from cachecontrol import CacheController
 from cachecontrol.cache import DictCache
+from cachecontrol.models import Response
 
 
 TIME_FMT = "%a, %d %b %Y %H:%M:%S GMT"
@@ -28,14 +32,13 @@ class NullSerializer(object):
 class TestCacheControllerResponse(object):
     url = "http://url.com/"
 
-    def req(self, headers=None):
-        headers = headers or {}
-        return Mock(full_url=self.url, url=self.url, headers=headers)  # < 1.x support
-
     def resp(self, headers=None):
         headers = headers or {}
-        return Mock(
-            status=200, headers=headers, request=self.req(), read=lambda **k: b"testing"
+        return Response(
+            200,
+            Headers(headers),
+            httpcore.PlainByteStream(b"testing"),
+            {}
         )
 
     @pytest.fixture()
@@ -50,20 +53,20 @@ class TestCacheControllerResponse(object):
 
         no_cache_codes = [201, 400, 500]
         for code in no_cache_codes:
-            resp.status = code
-            cc.cache_response(Mock(), resp)
+            resp.status_code = code
+            cc.cache_response(self.url, Headers(), resp, Mock())
             assert not cc.cache.set.called
 
         # this should work b/c the resp is 20x
-        resp.status = 203
-        cc.cache_response(self.req(), resp)
+        resp.status_code = 203
+        cc.cache_response(self.url, Headers(), resp, Mock())
         assert cc.serializer.dumps.called
         assert cc.cache.set.called
 
     def test_no_cache_with_no_date(self, cc):
         # No date header which makes our max-age pointless
         resp = self.resp({"cache-control": "max-age=3600"})
-        cc.cache_response(self.req(), resp)
+        cc.cache_response(self.url, Headers(), resp, Mock())
 
         assert not cc.cache.set.called
 
@@ -71,29 +74,30 @@ class TestCacheControllerResponse(object):
         # When the body is the wrong size, then we don't want to cache it
         # because it is obviously broken.
         resp = self.resp({"cache-control": "max-age=3600", "Content-Length": "5"})
-        cc.cache_response(self.req(), resp, b"0" * 10)
+        cc.cache_response(self.url, Headers(), resp, b"0" * 10)
 
         assert not cc.cache.set.called
 
     def test_cache_response_no_cache_control(self, cc):
         resp = self.resp()
-        cc.cache_response(self.req(), resp)
+        cc.cache_response(self.url, Headers(), resp, Mock())
 
         assert not cc.cache.set.called
 
     def test_cache_response_cache_max_age(self, cc):
         now = time.strftime(TIME_FMT, time.gmtime())
         resp = self.resp({"cache-control": "max-age=3600", "date": now})
-        req = self.req()
-        cc.cache_response(req, resp)
-        cc.serializer.dumps.assert_called_with(req, resp, None)
+
+        body = Mock()
+        cc.cache_response(self.url, Headers(), resp, body)
+        cc.serializer.dumps.assert_called_with(Headers(), resp, body)
         cc.cache.set.assert_called_with(self.url, ANY)
 
     def test_cache_response_cache_max_age_with_invalid_value_not_cached(self, cc):
         now = time.strftime(TIME_FMT, time.gmtime())
         # Not a valid header; this would be from a misconfigured server
         resp = self.resp({"cache-control": "max-age=3600; public", "date": now})
-        cc.cache_response(self.req(), resp)
+        cc.cache_response(self.url, Headers(), resp, Mock())
 
         assert not cc.cache.set.called
 
@@ -107,12 +111,12 @@ class TestCacheControllerResponse(object):
         resp = self.resp({"cache-control": "no-store"})
         assert cc.cache.get(cache_url)
 
-        cc.cache_response(self.req(), resp)
+        cc.cache_response(self.url, Headers(), resp, Mock())
         assert not cc.cache.get(cache_url)
 
     def test_cache_response_no_store_with_etag(self, cc):
         resp = self.resp({"cache-control": "no-store", "ETag": "jfd9094r808"})
-        cc.cache_response(self.req(), resp)
+        cc.cache_response(self.url, Headers(), resp, Mock())
 
         assert not cc.cache.set.called
 
@@ -120,16 +124,16 @@ class TestCacheControllerResponse(object):
         # Vary: * indicates that the response can never be served
         # from the cache, so storing it can be avoided.
         resp = self.resp({"vary": "*"})
-        cc.cache_response(self.req(), resp)
+        cc.cache_response(self.url, Headers(), resp, Mock())
 
         assert not cc.cache.set.called
 
     def test_update_cached_response_with_valid_headers(self):
-        cached_resp = Mock(headers={"ETag": "jfd9094r808", "Content-Length": 100})
+        cached_resp = Mock(headers=Headers({"ETag": "jfd9094r808", "Content-Length": "100"}))
 
         # Set our content length to 200. That would be a mistake in
         # the server, but we'll handle it gracefully... for now.
-        resp = Mock(headers={"ETag": "28371947465", "Content-Length": 200})
+        resp_headers = Headers({"ETag": "28371947465", "Content-Length": "200"})
         cache = DictCache({self.url: cached_resp})
 
         cc = CacheController(cache)
@@ -139,10 +143,10 @@ class TestCacheControllerResponse(object):
         cc.serializer.loads.return_value = cached_resp
         cc.cache_url = Mock(return_value="http://foo.com")
 
-        result = cc.update_cached_response(Mock(), resp)
+        result = cc.update_cached_response(self.url, Mock(), resp_headers)
 
-        assert result.headers["ETag"] == resp.headers["ETag"]
-        assert result.headers["Content-Length"] == 100
+        assert result.headers["ETag"] == resp_headers["ETag"]
+        assert result.headers["Content-Length"] == "100"
 
 
 class TestCacheControlRequest(object):
@@ -152,8 +156,7 @@ class TestCacheControlRequest(object):
         self.c = CacheController(DictCache(), serializer=NullSerializer())
 
     def req(self, headers):
-        mock_request = Mock(url=self.url, headers=headers)
-        return self.c.cached_request(mock_request)
+        return self.c.cached_request(self.url, Headers(headers))
 
     def test_cache_request_no_headers(self):
         cached_resp = Mock(headers={"ETag": "jfd9094r808", "Content-Length": 100}, status=200)
