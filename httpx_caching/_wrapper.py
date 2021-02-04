d523527628c50c4edf2b4c7680049ec132698230
Johannes <johtso@gmail.com>
2021-02-04 16:24:48 +0000

    Remove print statements

--- httpx_caching/_wrapper.py
+++ httpx_caching/_wrapper.py
@@ -7,17 +7,13 @@ from httpx_caching._types import AnyClient
 
 def CachingClient(client: AnyClient, *args, **kwargs) -> AnyClient:
     # TODO: Change this once HTTPX adds mounting functionality
-    print(client)
     current_transport = client._transport
-    print(current_transport)
     if "transport" not in kwargs:
         kwargs["transport"] = current_transport
 
     is_async = isinstance(current_transport, httpcore.AsyncHTTPTransport)
-    print(f"is async: {is_async}")
     client._transport = (AsyncCachingTransport if is_async else SyncCachingTransport)(
         *args, **kwargs
     )
-    print(client._transport)
 
     return client
